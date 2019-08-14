af4908e88b50140fe1e1efe419d293b3a3f4bbb3
xibeijia <xibeijia@xibeijia-PC>
2019-08-15 00:55:06 +0800

    duoduiduo

--- src/com/how2java/TestMybatis.java
+++ src/com/how2java/TestMybatis.java
@@ -9,6 +9,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
  
+import com.how2java.pojo.Order;
+import com.how2java.pojo.OrderItem;
 import com.how2java.pojo.Product;
   
 public class TestMybatis {
@@ -19,13 +21,43 @@ public class TestMybatis {
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession session = sqlSessionFactory.openSession();
   
-        List<Product> ps = session.selectList("listProduct");
-        for (Product p : ps) {
-            System.out.println(p+" 对应的分类是 \t "+ p.getCategory());
-        }
+        deleteOrderItem(session);
+//        addOrderItem(session);
+        listOrder(session);
  
         session.commit();
         session.close();
   
+    }
+ 
+    private static void deleteOrderItem(SqlSession session) {
+        Order o1 = session.selectOne("getOrder",1);
+        Product p6 = session.selectOne("getProduct",6);
+        OrderItem oi = new OrderItem();
+        oi.setProduct(p6);
+        oi.setOrder(o1);
+        session.delete("deleteOrderItem", oi);     
+    }
+ 
+    private static void addOrderItem(SqlSession session) {
+        Order o1 = session.selectOne("getOrder",1);
+        Product p6 = session.selectOne("getProduct",6);
+        OrderItem oi = new OrderItem();
+        oi.setProduct(p6);
+        oi.setOrder(o1);
+        oi.setNumber(200);
+         
+        session.insert("addOrderItem", oi);
+    }
+ 
+    private static void listOrder(SqlSession session) {
+        List<Order> os = session.selectList("listOrder");
+        for (Order o : os) {
+            System.out.println(o.getCode());
+            List<OrderItem> ois= o.getOrderItems();
+            for (OrderItem oi : ois) {
+                System.out.format("\t%s\t%f\t%d%n", oi.getProduct().getName(),oi.getProduct().getPrice(),oi.getNumber());
+            }
+        }
     }
 }
