46c39e218092dcf7216343e8181ac238ca128207
xibeijia <xibeijia@xibeijia-PC>
2019-08-17 00:40:39 +0800

    nixianggongcheng

--- src/com/how2java/TestMybatis.java
+++ src/com/how2java/TestMybatis.java
@@ -1,30 +1,42 @@
 package com.how2java;
 
-import java.io.IOException;
 import java.io.InputStream;
+
 import java.util.List;
 
+//import com.how2java.mapper.CategoryMapper;
+
+//import com.how2java.pojo.Category;
+//import com.how2java.pojo.CategoryExample;
+//import com.how2java.mapper.CategoryMapper;
+//import com.how2java.pojo.CategoryExample;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import com.how2java.pojo.Category;
+
 
 public class TestMybatis {
+    public static void main(String args[]) throws Exception {
+        System.out.println("先运行TestMybatisGenerator创建mapper,pojo,xml 等文件，然后取消import里被注释的，以及接下来的注释，并执行代码");
 
-	public static void main(String[] args) throws IOException {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		SqlSession session = sqlSessionFactory.openSession();
-		List<Category> cs =session.selectList("listCategory");
-		for (Category c : cs) {
-			System.out.println(c.getName());
-		}
 
-		session.close();
-		
-}
+//        String resource = "mybatis-config.xml";
+//        InputStream inputStream = Resources.getResourceAsStream(resource);
+//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//        SqlSession session=sqlSessionFactory.openSession();
+//        CategoryExample example = new CategoryExample();
+//        example.createCriteria().andNameLike("%9%");
+//        CategoryMapper mapper = session.getMapper(CategoryMapper.class);
+//        List<Category> cs= mapper.selectByExample(example);
+//        for (Category c : cs) {
+//            System.out.println(c.getName());
+//        }
 
+
+        
+
+
+    }
 }
